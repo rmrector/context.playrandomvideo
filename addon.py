a790f4cc48c4d2f5057c77f76b02fe80e34b6bbc
Ryan Rector <rmrector@gmail.com>
2015-06-28 11:19:55 -0600

    Now works on seasons and episode smart playlists

--- addon.py
+++ addon.py
@@ -29,38 +29,99 @@
 # videodb://tvshows/titles/1480/2/?tvshowid=1480&xsp={"order":{"direction":"ascending","ignorefolders":0,"method":"sorttitle"},"type":"tvshows"}
 # videodb://tvshows/titles/1580/?filter={"rules":{"and":[{"field":"rating","operator":"between","value":["9.5","10"]}]},"type":"tvshows"}&xsp={"order":{"direction":"ascending","ignorefolders":0,"method":"sorttitle"},"type":"tvshows"}
 
+import json
+import urllib
 import xbmc
 import xbmcgui
-import json
+import xbmcvfs
 
-def main():
-    if xbmc.getCondVisibility("Container.Content(tvshows)"):
-        tvshowid = xbmc.getInfoLabel("ListItem.DBID")
-        
-        rawJsonEpisodes = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodes", "params": {"tvshowid": %s, "properties": ["file", "episode", "season", "plot"], "sort": {"method": "random"}, "limits": {"end": 20}}, "id": 1}' % tvshowid)
-        jsonEpisodes = json.loads(rawJsonEpisodes)
-        
-        print rawJsonEpisodes
-        
+import xml.etree.ElementTree as ET
+
+LIMIT = 100
+
+tvshowTitlesPath = "videodb://tvshows/titles/"
+
+debugMessage = None
+
+def randomList(path):
+    global debugMessage
+
+    print path
+    xbmc.executebuiltin('Skin.SetString(debug.message, "%s")' % path)
+
+    playlistExtension = ".xsp"
+    if (path.startswith(tvshowTitlesPath)):
+        playTvShow(path)
+    elif(path.endswith(playlistExtension)):
+        playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+        playlist.clear()
+        playlistLoaded = False
+
+        playlistXml = xbmcvfs.File(path, 'r')
+        playlistXml = playlistXml.read()
+        playlistXml = ET.fromstring(playlistXml)
+        if playlistXml.tag == 'smartplaylist' and 'type' in playlistXml.attrib:
+            if playlistXml.attrib['type'] == 'episodes':
+                episodeIds = xbmcvfs.listdir(path)[1]
+                for episodeId in episodeIds:
+                    jsonRequest = {"jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodeDetails", "id": 1}
+                    jsonRequest["params"] = {"episodeid": int(episodeId), "properties": ["file"]}
+                    episode = json.loads(xbmc.executeJSONRPC(json.dumps(jsonRequest)))
+                    episode = episode["result"]["episodedetails"]
+                    listItem = xbmcgui.ListItem(episode["label"])
+                    playlist.add(episode["file"], listItem)
+                    print episode["label"]
+                playlistLoaded = True
+            else:
+                debugMessage = "[context.playrandom] Unsupported playlist! '%s'" % path
+        else:
+            debugMessage = "[context.playrandom] Unsupported playlist! '%s'" % path
+
+        if playlistLoaded:
+            playlist.shuffle()
+            xbmc.Player().play(playlist)
+            debugMessage = "[context.playrandom] Successfully random'd '%s'" % path
+    else:
+        debugMessage = "[context.playrandom] Unsupported path! %s" % path
+
+    if debugMessage:
+        xbmc.log("RECTOR: %s" % debugMessage)
+        xbmc.executebuiltin('SetProperty(debug.message, "%s", 50)' % debugMessage)
+
+def playTvShow(path):
+    originalPath = path
+    path = path[len(tvshowTitlesPath):].split('?')[0].split('/')
+
+    tvshowId = path[0]
+    season = path[1] if len(path) > 1 else None
+
+    jsonRequest = {"jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodes", "id": 1}
+    jsonRequest["params"] = {"properties": ["file"], "sort": {"method": "random"}, "limits": {"end": LIMIT}}
+
+    if tvshowId:
+        jsonRequest["params"]["tvshowid"] = int(tvshowId)
+    if season:
+        jsonRequest["params"]["season"] = int(season)
+
+    jsonEpisodes = xbmc.executeJSONRPC(json.dumps(jsonRequest))
+    jsonEpisodes = json.loads(jsonEpisodes)
+
+    if len(jsonEpisodes["result"]["episodes"]):
         playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
         playlist.clear()
         for episode in jsonEpisodes["result"]["episodes"]:
             listItem = xbmcgui.ListItem(episode["label"])
             playlist.add(episode["file"], listItem)
-        
-        episodeCount = jsonEpisodes["result"]["limits"]["total"]
-        
-        message = "TV Show: %s (%s)" % (tvshowid, episodeCount)
-        
+
         xbmc.Player().play(playlist)
+        debugMessage = "[context.playrandom] Successfully random'd '%s'" % originalPath
     else:
-        path = urllib.unquote(xbmc.getInfoLabel("ListItem.FolderPath"))
-        message = "Unsupported path!: %s" % path
-        
-    xbmc.executebuiltin("Skin.SetString(debug.message, \"%s\")" % message)
-    xbmc.log("RECTOR: %s" % message)
-    
+        debugMessage = "[context.playrandom] Couldn't find any episodes to random"
+
+def main():
+    randomList(urllib.unquote(xbmc.getInfoLabel("ListItem.FolderPath")))
     return
+
     if xbmc.getCondVisibility("Container.Content(movies)"):
         xbmc.executebuiltin("RunScript(script.extendedinfo,info=extendedinfo,dbid=%s,id=%s)" % (xbmc.getInfoLabel("ListItem.DBID"), xbmc.getInfoLabel("ListItem.Property(id)")))
     elif xbmc.getCondVisibility("Container.Content(tvshows)"):
