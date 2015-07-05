336df015bd0ec708786571d32dfae6cdbe0504f9
Ryan Rector <rmrector@gmail.com>
2015-07-04 21:34:01 -0600

    pass media type to script

--- context.py
+++ context.py
@@ -20,7 +20,7 @@ def log(message, log_to_gui=True, level=xbmc.LOGDEBUG):
         # Yeah, this is ugly, so def want it to be a module
         builtin = 'RunScript(script.design.helper, log, %s, "%s"' % (__addonid__, message)
         if log_to_gui:
-            builtin += ', log_to_gui'
+            builtin += ', logToGui'
         builtin += ')'
         xbmc.executebuiltin(builtin.encode('utf-8'))
     else:
@@ -28,8 +28,19 @@ def log(message, log_to_gui=True, level=xbmc.LOGDEBUG):
 
 
 def main():
-    full_url = xbmc.getInfoLabel("ListItem.FolderPath").decode("utf-8")
-    builtin = 'RunScript(script.playrandom, "%s")' % full_url
+    path = xbmc.getInfoLabel("ListItem.FolderPath").decode("utf-8")
+    if xbmc.getCondVisibility('Window.IsVisible(MyVideoNav.xml)'):
+        media = 'video'
+    elif xbmc.getCondVisibility('Window.IsVisible(MyMusicNav.xml)') or xbmc.getCondVisibility('Window.IsVisible(MyMusicSongs.xml)'):
+        media = 'music'
+    else:
+        media = None
+        log("Don't know what media type to pass for path '%s'" % path)
+
+    if media:
+        builtin = 'RunScript(script.playrandom, "%s", %s)' % (path, media)
+    else:
+        builtin = 'RunScript(script.playrandom, "%s")' % path
     xbmc.executebuiltin(builtin.encode('utf-8'))
 
 
