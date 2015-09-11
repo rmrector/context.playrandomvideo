39ef6753d87b855c9941ca4a7c0d473cb8a886d1
Ryan Rector <rmrector@gmail.com>
2015-09-11 17:48:25 -0600

    Focus on video

--- context.py
+++ context.py
@@ -1,22 +1,9 @@
-from devhelper import pykodi
-from devhelper.pykodi import log
-
-def main():
-    path = pykodi.get_infolabel('ListItem.FolderPath')
-    if pykodi.get_conditional('Window.IsVisible(MyVideoNav.xml)'):
-        media = 'video'
-    elif pykodi.get_conditional('Window.IsVisible(MyMusicNav.xml)') or pykodi.get_conditional('Window.IsVisible(MyMusicSongs.xml)'):
-        media = 'music'
-    else:
-        media = None
-        log("Don't know what media type to pass for path '%s'" % path, xbmc.LOGWARNING)
-
-    if media:
-        builtin = 'RunScript(script.playrandom, "%s", %s)' % (path, media)
-    else:
-        builtin = 'RunScript(script.playrandom, "%s")' % path
-
-    pykodi.execute_builtin(builtin)
+import sys
+import xbmc
 
 if __name__ == '__main__':
-    main()
+    path = sys.listitem.getfilename()
+
+    label = sys.listitem.getLabel()
+    builtin = 'RunScript(script.playrandomvideos, "%s", label="%s")' % (path, label)
+    xbmc.executebuiltin(builtin)
