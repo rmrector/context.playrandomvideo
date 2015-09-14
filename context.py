14247a169ebe477ec361c9f322f68282dbdf5c42
Ryan Rector <rmrector@gmail.com>
2015-09-14 12:45:10 -0600

    Pass label to script properly (fixes TV Actors/Studios/Tags)

--- context.py
+++ context.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     path = sys.listitem.getfilename()
 
     label = sys.listitem.getLabel()
-    builtin = 'RunScript(script.playrandomvideos, "%s", label="%s")' % (path, label)
+    builtin = 'RunScript(script.playrandomvideos, "%s", "label=%s")' % (path, label)
     xbmc.executebuiltin(builtin)
